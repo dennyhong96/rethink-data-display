6779bb17b5d0500e985b6cab0f41474ffc95e651
Denny Hong <hong961127@gmail.com>
2021-03-20 12:38:51 -0700

    Configure Material ui

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-	setupFilesAfterEnv: ["./jest.setup.js"]
+	setupFilesAfterEnv: ["./jest.setup.js"],
+	moduleNameMapper: {
+		"^@components(.*)$": "<rootDir>/src/components$1",
+		"^@lib(.*)$": "<rootDir>/src/lib$1",
+		"^@pages(.*)$": "<rootDir>/src/pages$1",
+		"^@styles(.*)$": "<rootDir>/src/styles$1",
+	},
 };
